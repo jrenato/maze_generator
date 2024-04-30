e5b16098a44ba28069722ea9b0b2c02443385289
karvalho <renato@karvalho.com.br>
2024-04-30 15:56:54 -0300

    Set tilemap example for maze generation

--- Levels/maze_2d.gd
+++ Levels/maze_2d.gd
@@ -1,8 +1,30 @@
 extends Node2D
 
+@export var floor_tile: Vector2i = Vector2i(0, 4)
+@export var wall_tile: Vector2i = Vector2i(4, 3)
+
+@onready var maze_generator: Node = %MazeGenerator
 @onready var tile_map: TileMap = %TileMap
 
 
 func _ready() -> void:
-	var first_cell: Vector2i = Vector2i(0, 0)
-	tile_map.set_cell(0, first_cell, 0, Vector2i(1, 0))
+	maze_generator.maze_generated.connect(_on_maze_generated)
+	maze_generator.generate_maze()
+
+
+func fill_tile_map() -> void:
+	tile_map.clear()
+	for x in range(maze_generator.width):
+		for y in range(maze_generator.height):
+			set_cell(x, y, maze_generator.maze_get(x, y))
+
+
+func set_cell(x: int, y: int, value: int) -> void:
+	if value == 0:
+		tile_map.set_cell(0, Vector2i(x, y), 0, floor_tile)
+	else:
+		tile_map.set_cell(0, Vector2i(x, y), 0, wall_tile)
+
+
+func _on_maze_generated() -> void:
+	fill_tile_map()
